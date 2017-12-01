30ee6905ab1070952812a1977d0d1af7c9e4bf34
何彪123 <644675356@qq.com>
2017-12-01 11:57:49 +0800

    回去接着看

---
+++ src/main/java/com/xiaozl/initialwork1/filter/LoginFilter.java
@@ -0,0 +1,32 @@
+package com.xiaozl.initialwork1.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.Set;
+
+/**
+ * LoginFilter
+ *
+ * @author Daydreamer
+ * @date 2017/11/29 14:00
+ */
+public class LoginFilter implements Filter {
+    private FilterConfig filterConfig;
+
+    public void init(FilterConfig filterConfig){
+        System.out.println("初始化");
+        this.filterConfig = filterConfig;
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Demo1过滤前");
+        System.out.println(filterConfig.getInitParameter("param1"));//具体配置在web.xml里
+        chain.doFilter(request, response);
+        System.out.println("Demo1过滤后");
+    }
+
+    public void destroy(){
+        System.out.println("销毁");
+    }
+
+}
