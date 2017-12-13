783305a967efa69c138ec92ed6d0dd1aa81e398f
何彪123 <644675356@qq.com>
2017-12-13 20:04:15 +0800

    刚刚不知道哪出错，所有的都无法自动装配了，现在好了，好像是mapper里面不小心改错了哪里。

--- src/main/java/com/xiaozl/initialwork1/mapper/UserMapper.java
+++ src/main/java/com/xiaozl/initialwork1/mapper/UserMapper.java
@@ -1,9 +1,7 @@
 package com.xiaozl.initialwork1.mapper;
 
-import org.apache.ibatis.annotations.Param;
-
 import com.xiaozl.initialwork1.entity.User;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,41 +11,14 @@ import java.util.List;
  */
 public interface UserMapper {
 
-    /**
-     * 添加新用户
-     *
-     * @param user
-     * @throws Exception
-     */
-
     public void newUser(@Param("user") User user) throws Exception;
 
-    /**
-     * 删除用户（状态设为0）
-     *
-     * @param userId
-     * @throws Exception
-     */
     public void delUser(@Param("userId") Integer userId) throws Exception;
 
-    /**
-     * 修改用户信息
-     *
-     * @param user
-     * @throws Exception
-     */
     public void updateUser(@Param("user") User user) throws Exception;
 
-    /**
-     * 查询所有的用户信息
-     *
-     * @return
-     * @throws Exception
-     */
-    @Select("listUser")
     public List<User> listUser() throws Exception;
 
     public int countByUserNameAndPassword(@Param("userName") String userName,
                                           @Param("password") String password) throws Exception;
-
 }
